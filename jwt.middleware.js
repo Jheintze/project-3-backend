4a1560bb4807d6669f7dd60bf3fc41be29c2fae2
Jheintze <jheintze33@gmail.com>
2024-11-06 17:40:06 +0100

    Add files via upload

--- jwt.middleware.js
+++ jwt.middleware.js
@@ -4,7 +4,7 @@ const { expressjwt: jwt } = require("express-jwt");
 
 // Instantiate the JWT token validation middleware
 const isAuthenticated = jwt({
-  secret: mySecret,
+  secret: process.env.TOKEN_SECRET,
   algorithms: ["HS256"],
   requestProperty: "payload",
   getToken: getTokenFromHeaders,
