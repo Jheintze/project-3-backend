df60258cddf7e120bc688858bdf17199a97a4812
Jheintze <jheintze33@gmail.com>
2024-11-08 18:15:26 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 // ℹ️ Connects to the database
 require("./db");
 
-
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
@@ -15,19 +14,57 @@ const cors = require("cors");
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-// Set the origin dynamically based on the environment
-const allowedOrigins =
-  process.env.NODE_ENV === "production"
-    ? "https://nextleveltravel.netlify.app" // production URL
-    : "http://localhost:3000"; // local development URL
+// const allowedOrigins = (
+//   process.env.NODE_ENV === "production"
+//     ? "https://nextleveltravel.netlify.app"
+//     : "http://localhost:3000"
+// ).trim();
 
+//     console.log("Allowed Origins:", allowedOrigins);
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Allow requests if origin matches allowedOrigins exactly
+//       if (origin === allowedOrigins || origin === undefined) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true, // Allow credentials (cookies, tokens, etc.)
+//   })
+// );
 app.use(
   cors({
-    origin: allowedOrigins, // Allow the correct frontend URL
-    credentials: true, // Allow credentials (cookies, tokens, etc.)
+    origin: "https://nextleveltravel.netlify.app",
+    credentials: true,
   })
 );
 
+
+app.options("*", cors());
+
+console.log("Allowed FRONTEND_URL:", process.env.FRONTEND_URL);
+console.log("Current NODE_ENV:", process.env.NODE_ENV);
+
+// 👇 Start handling routes here
+const indexRoutes = require("./routes/index.routes");
+app.use("/api", indexRoutes);
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
+
+const planetRoutes = require("./routes/planet.routes");
+app.use("/api", planetRoutes);
+
+const flightRoutes = require("./routes/flights.routes");
+app.use("/api", flightRoutes);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+module.exports = app;
 
 
 // const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:3000"];
@@ -54,34 +91,6 @@ app.use(
 //     credentials: true,
 //   })
 // );
-
-console.log("Allowed FRONTEND_URL:", process.env.FRONTEND_URL);
-
-// app.use(
-//     cors({
-//          origin:process.env.FRONTEND_URL,
-//       credentials: true,
-      
-//     })
-//   );
-
-// 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
-
-const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
-
-const planetRoutes = require("./routes/planet.routes");
-app.use("/api", planetRoutes);
-
-const flightRoutes = require("./routes/flights.routes");
-app.use("/api", flightRoutes);
-
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
-
 // app.use(
 //   cors({
 //     origin: process.env.FRONTEND_URL,
@@ -89,5 +98,3 @@ require("./error-handling")(app);
 //     origin: "*"
 //   })
 // );
-
-module.exports = app;
